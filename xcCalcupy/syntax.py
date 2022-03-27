83b3fe3cd4ce4cccf0df88f517686df5f3f047af
Ron4-kw0rk3r <101228049+Ron4-kw0rk3r@users.noreply.github.com>
2022-03-26 23:18:26 -0400

    Add files via upload
    how very good.  off time in is mmmm, uff code comprovating symbol is argument in CLI

---
+++ xcCalcupy/syntax.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env pỳthon3.8
+# for_one_loops_now_code _Abb
+
+import sys
+from sys import stdin 
+import math
+                
+code = ["+","-","/", "*", '//', '%', '**', '¶']
+# programing detector __ how syntax in arguments in code beutifull is better code _name _test_ haweber _name _is a longer_ time code sys its exit 
+# how way bey home is better now restrics one punch now out  
+# it is crazy now contrib mallock now faster 
+if  __name__ == '__main__':
+    def comprovatting(rawx, sym, rawy):
+        vag = 0
+    # for i in ['0','1','2','3','4','5','6','7','8', '9', '10']:
+        for i in ["+","-","/",'//', "*", '%', '**', '¶', ')','(']:
+            print('value increement list it is #{} in signs {}'.format(vag, i))
+            vag += 1
+            # how example it is string.count(string, star_index, end_index)
+            # c = code.count("+")
+            #num = 0
+            # num += 1
+                        
+            #print('signs it is :#{} is number {}'.format( ~? ,i))
+            # #print('signs it is :#{} is number {}'.format( ~? ,i))
+            # ints = [8, 23, 45, 12, 78]
+            # index =0
+            # # for z in [0,1,'+',3,4,5] :
+                # print(z, index)
+                # index += 1 # se le pone de bajo del print para que el ciclo buelva a empezar
+                # if index == 5:
+                    # sys.exit(0)
+                # #print('item #{} = {}'.format('???', z))
+            # 
+            # for x in range(0, len(i)):
+            # for z in range(0, enumerate(i)):
+            # def ron4():
+                # numberup = 0
+                # # numberup = 0
+                # for masive in range(0, 11):
+                    # numberup += 1
+                    # if masive >= 10: break
+                    # print('it is list object  {} number off {}'.format(i, numberup))
+                # return numberup
+            # ron4()
+                # masive, numberup)
+                # masive += 1
+                # ron4(code)
+                # print('numbers is list is ;', )
+                # print(z)
+                #print('\t', x)
+                # for k in len(int(x[0:1])):
+                # if x == 5 and   == x :
+                # if range(len(i)) and x == 2:
+                    # #print(it)
+                    # print(i)
+                    # sys.exit(0)
+                # # elif x == len(1)
+            # for k in str(len(i[2])):
+                #print(k)
+
+            # for x in range(0, 16):
+            # for k in str(len(i)):
+                # print(k)
+  #              return k
+                # print('name singno  is pocition ', x)
+                # for x in range(len(i)):
+                # print('name singno {} is pocition {}').format(i, x)
+                # print('name singno is posicition {}').format( x)
+                # return x
+                # if sym == i:
+                    # if i == '+':
+                        # print( "sign select >----->[{}]".format(i))
+                        # funx_0 = eval('rawx + rawy')
+                        # print( "result sign  >-->[{}] it is result [{}]".format(i, funx_0))
+                       # # sys.exit(0)
+                        # #return x
+                    # elif i == '-':
+                        # print( "sign select >----->[{}]".format(i))
+                        # fnx_1 = eval('rawx - rawy')
+                        # print( "result sign  >-->[{}] it is result [{}]".format(i, fnx_1))
+                        # # funx_0 = (rawx i rawy)
+                       # # sys.exit(0)
+                    # 
+            # if i == rawx:
+            # if i == sym :
+                # funx_0 = eval('rawx + rawy')
+#                return i
+                # for x in ["+","-","/", "*", '//', '%', '**', '¶', ')','(' ]:
+                    # if x == rawy:
+                        # print(x)
+                        # return x
+            # print(i)
+           # if i == str('+'):
+           #     code='+'
+           #     print("el signo es {}".format(code))
+           # elif i == str('-'):
+           #     cod='-'
+           #     print("el signo es {}".format(cod))
+                
+                
+                #print('uno ', len(i[0:]))
+            # if len(i[0:0]) == rawx:
+            # if len(i[1]) == rawx:
+                # print("uname _>>>> forget_calculator : ")
+                # print(rawx )
+                # sys.exit(0)
+            
+            # elif len(i[1]) == rawy:
+            #     print('rawy' +  i + '\n' )
+#                print('rawx' + i '\n' )
+
+    comprovatting(int(sys.argv[1]), str(sys.argv[2]), int(sys.argv[3]))
